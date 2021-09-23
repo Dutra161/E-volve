31f0ff3b0d3aaeac5f2e5abe786a97ba573f5bc3
Dutra <gabriel.d.o@hotmail.com.br>
2021-09-23 19:05:16 -0300

    evolve

--- application/controllers/Download.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Download extends CI_Controller {
-	public function index()
-	{
-		$this->template->show('download');
-	}
-}
--- application/controllers/Results.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Results extends CI_Controller {
-	public function index()
-	{
-		$this->load->view('results');
-	}
-}
